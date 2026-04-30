14854af66da8950b2a94f6cb0aa26f92850b4b2f
simoncorrea1026-sketch <simoncorrea1026@gmail.com>
2026-04-29 20:16:45 -0500

    Add Taylor.h with function declarations

---
+++ Taylor.h
@@ -0,0 +1,10 @@
+#ifndef TAYLOR_H
+#define TAYLOR_H
+
+double factorial(int n);
+double senoTaylor(double x, int n);
+double cosenoTaylor(double x, int n);
+double exponencialTaylor(double x, int n);
+double logaritmoTaylor(double x, int n);
+
+#endif
